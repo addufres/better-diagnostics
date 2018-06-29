1755a56934a1c6971a81a587cb034a54e4ee51f1
AndrewD <adufresplatinum@gmail.com>
2018-06-29 10:50:34 -0600

    got firebase cooperating with listView and displaying the list of troubleCodes working on click event to take the user to a new view to display the description of the code now

--- better-diagnostics/app/src/main/java/com/thedufresnegroup/adufres/betterdiagnostics/MainActivity.java
+++ better-diagnostics/app/src/main/java/com/thedufresnegroup/adufres/betterdiagnostics/MainActivity.java
@@ -8,36 +8,68 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-
+/*
+   Primary purpose of the main screen is to list all of the codes
+   available to read and then click each code to view more detailed
+   information about each code
+*/
 public class MainActivity extends AppCompatActivity {
     private ListView mainListView;
-    private DatabaseReference mDatabase;
+    private DatabaseReference dbRef;
     private ArrayAdapter<String> listAdapter ;
+    private ArrayList<String> codeList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("TroubleCode");
-        // Find the ListView resource.
-        mainListView = findViewById( R.id.mainListView );
-
+        mainListView = findViewById( R.id.mainListView ); // Find the ListView resource.
+        listAdapter = new ArrayAdapter<>(this, R.layout.simplerow, codeList); // Create ArrayAdapter using the TroubleCode list.
+        mainListView.setAdapter( listAdapter ); // Set the ArrayAdapter as the ListView's adapter.
+        dbRef = FirebaseDatabase.getInstance().getReference().child("TroubleCode"); // Get FireBase child ref to run through i.e "collection"
+        /*
+            This event listener pulls in the data from Firebase and
+            takes puts them through a forEach loop storing each child
+            in a POJO and taking the information and displaying the code
+            as a string to the user each code as its own list item in listView
+        */
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    //Getting the data from snapshot
+                    TroubleCode tc = postSnapshot.getValue(TroubleCode.class);
+                    //Adding it to a string
+                    String code = tc.getCode();
+                    codeList.add(code);
+                    // check for size change and adjust adapter and view
+                    if(codeList.size() == 1) {
+                        listAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1,codeList);
+                        mainListView.setAdapter(listAdapter);
+                    } else if (codeList.size() > 1) {
+                        listAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        });
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        ArrayList<String> codeList = new ArrayList<String>();
-        codeList.addAll(Arrays.asList(mDatabase.toString()));
-
-        // Create ArrayAdapter using the TroubleCode list.
-        listAdapter = new ArrayAdapter<>(this, R.layout.simplerow, codeList);
-        // Set the ArrayAdapter as the ListView's adapter.
-        mainListView.setAdapter( listAdapter );
+        /*
+            I still need to add action for when you click on a listView
+            item to take you to a detailed view of that trouble code and
+            have it display all of the information
+        */
     }
 
     @Override
